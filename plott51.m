5bd6b449dd9d9f25fadf0a92aa9b83473a63386a
Harper Chisari <harper.chisari@gmail.com>
2019-03-13 13:04:49 -0400

    Interp reaction update 3
    Works in x and z, but not y for some reason. modified Eofat to fix the self-acting particle issue more concretely. All charges of q=0 have an E=0.

--- plott51.m
+++ plott51.m
@@ -1,58 +1,64 @@
 function [Plott]=plott51(a,b,c,q)
-  h=.05;
+  h=.005;
 
-  x_lol= 0:h:6;
+  x_lol= 0:h:4;
 
-  q_p=[1,1];
-  m=[1,1];
-  x_o=[0,0];
-  y_o=[0,1];
-  z_o=[0,0];
-  u_o=[0,0];
-  v_o=[0,0];
-  w_o=[0,0];
+  q_p=[0,1,-1];
+  m=[1,1,1];
+  x_o=[0,0,0];
+  y_o=[0,1,-1];
+  z_o=[0,0,0];
+  u_o=[0,0,0];
+  v_o=[0,0,0];
+  w_o=[0,0,0];
   
-P(1,1,:)=x_o;
-P(1,2,:)=y_o;
-P(1,3,:)=z_o;
-P(1,4,:)=u_o;
-P(1,5,:)=v_o;
-P(1,6,:)=w_o;
+P(:,1,1)=x_o;
+P(:,2,1)=y_o;
+P(:,3,1)=z_o;
+P(:,4,1)=u_o;
+P(:,5,1)=v_o;
+P(:,6,1)=w_o;
 
   [ m_m, n_m] = size(m);
-
+n_m
   [ m_xlol, n_xlol] = size(x_lol);
 
 ##particles will move one step after landing in another particle
 
 for i=2:n_xlol
   for p=1:n_m
-   Fix=zeros(size(P(i-1,1,:)));
+   Fix(1,n_m)=0;
    Fiy=Fix;
    Fiz=Fiy;
    Fiq=Fiz;
-   Fix(p)=P(i-1,1,:)(p)+1;
-   Fiy(p)=P(i-1,2,:)(p)+1;
-   Fiz(p)=P(i-1,3,:)(p)+1;
-   Fiq(p)=q_p(p);
-   Fiq
-   q_p
-   q_p-Fiq
-   P(i-1,6,p)
-   P(i,1,p)=P(i-1,1,p)+P(i-1,4,p)*(h);
-   P(i,2,p)=P(i-1,2,p)+P(i-1,5,p)*(h);
-   P(i,3,p)=P(i-1,3,p)+P(i-1,6,p)*(h);
-   P(i,4,p)=P(i-1,4,p)+(q_p(p))*(Eofat(P(i-1,1,:)-Fix,P(i-1,2,:)-Fiy,P(i-1,3,:)-Fiz,q_p-Fiq,P(i-1,1,p),P(i-1,2,p),P(i-1,3,p))(1)/m(p))*(h);
-   P(i,5,p)=P(i-1,5,p)+(q_p(p))*(Eofat(P(i-1,1,:)-Fix,P(i-1,2,:)-Fiy,P(i-1,3,:)-Fiz,q_p-Fiq,P(i-1,1,p),P(i-1,2,p),P(i-1,3,p))(2)/m(p))*(h);
-   P(i,6,p)=P(i-1,6,p)+(q_p(p))*(Eofat(P(i-1,1,:)-Fix,P(i-1,2,:)-Fiy,P(i-1,3,:)-Fiz,q_p-Fiq,P(i-1,1,p),P(i-1,2,p),P(i-1,3,p))(3)/m(p))*(h);
-  end
+   Fiq(1,p)=q_p(p);
+   P(p,1,i-1);
+   P(p,1,i)=P(p,1,i-1)+P(p,4,i-1)*(h);
+   P(p,2,i)=P(p,2,i-1)+P(p,5,i-1)*(h);
+   P(p,3,i)=P(p,3,i-1)+P(p,6,i-1)*(h);
+   P(p,4,i)=P(p,4,i-1)+(q_p(p))*(Eofat(P(:,1,i-1)-Fix,P(:,2,i-1)-Fiy,P(:,3,i-1)-Fiz,q_p-Fiq,P(p,1,i-1),P(p,2,i-1),P(p,3,i-1))(1)/m(p))*(h);
+   P(p,5,i)=P(p,5,i-1)+(q_p(p))*(Eofat(P(:,1,i-1)-Fix,P(:,2,i-1)-Fiy,P(:,3,i-1)-Fiz,q_p-Fiq,P(p,1,i-1),P(p,2,i-1),P(p,3,i-1))(2)/m(p))*(h);
+   P(p,6,i)=P(p,6,i-1)+(q_p(p))*(Eofat(P(:,1,i-1)-Fix,P(:,2,i-1)-Fiy,P(:,3,i-1)-Fiz,q_p-Fiq,P(p,1,i-1),P(p,2,i-1),P(p,3,i-1))(3)/m(p))*(h);
+   Fix=zeros(size(Fix));
+   Fiy=zeros(size(Fix));
+   Fiz=zeros(size(Fix));
+   Fiq=zeros(size(Fix));
+   end
 end   
 
 quiver3(0,0,0,0,0,0)
 hold on
 axis(3*[-1,1,-1,1,-1,1])
+for i=1:n_xlol
 for p=1:n_m
-plot3(P(:,1,p),P(:,2,p),P(:,3,p))
+ forn(p,i)=P(p,1,i);
+ form(p,i)=P(p,2,i);
+ forp(p,i)=P(p,3,i);
+end
+end
+
+for p=1:n_m
+plot3(forn(p,:),form(p,:),forp(p,:))
 end
 
 hold off
